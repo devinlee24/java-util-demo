02a205a37390e475d2eb2ffdae2ecb780235965a
devinlee <devinlee24@foxmail.com>
2019-12-17 22:45:12 +0800

    返回结果封装

--- src/main/java/com/devinlee/javautildemo/util/ResultInfo.java
+++ src/main/java/com/devinlee/javautildemo/util/ResultInfo.java
@@ -1,21 +1,67 @@
 package com.devinlee.javautildemo.util;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
 /**
  * 返回结果封装
+ *
+ * @author devinlee
  */
-public class ResultInfo<T> implements Serializable {
+@Data
+public class ResultInfo implements Serializable {
 
-    private Integer retCode;
+    // 状态码
+    private Integer code;
 
-    private String retMsg;
+    // 返回消息
+    private String message;
 
-    private T data;
+    // 返回数据
+    private Object data;
 
-    public ResultInfo(Integer retCode, String retMsg, T data) {
-        this.retCode = retCode;
-        this.retMsg = retMsg;
+    public ResultInfo(Integer code, String message, Object data) {
+        this.code = code;
+        this.message = message;
         this.data = data;
+    }
+
+    // 统一返回
+    private static ResultInfo returnData(Integer code, String message, Object data) {
+
+        return new ResultInfo(code, message, data);
+    }
+
+    // SUCCESS
+    public static ResultInfo success() {
+
+        return returnData(Response.SUCCESS.code(), Response.SUCCESS.message(), null);
+    }
+
+    public static ResultInfo success(Object data) {
+
+        return returnData(Response.SUCCESS.code(), Response.SUCCESS.message(), data);
+    }
+
+    public static ResultInfo success(String message, Object data) {
+
+        return returnData(Response.SUCCESS.code(), message, data);
+    }
+
+    // ERROR
+    public static ResultInfo fail() {
+
+        return returnData(Response.ERROR.code(), Response.ERROR.message(), null);
+    }
+
+    public static ResultInfo fail(Integer code, String message) {
+
+        return returnData(code, message, null);
+    }
+
+    public static ResultInfo fail(Integer code, String message, Object data) {
+
+        return returnData(code, message, data);
     }
 }
