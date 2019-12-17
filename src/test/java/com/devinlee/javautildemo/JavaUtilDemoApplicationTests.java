eb09db2ee56095328e219e8e6bc443f5b8746f1c
devinlee <devinlee24@foxmail.com>
2019-12-17 10:36:25 +0800

    初始化

---
+++ src/test/java/com/devinlee/javautildemo/JavaUtilDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinlee.javautildemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUtilDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
